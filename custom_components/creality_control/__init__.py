f3eb2df74f82f09edc190f9e9925a2a465b8c706
Luis Rodriguez <me@luisdrodriguez.com>
2025-10-11 12:11:08 -0400

    Fixed HA deprecations

--- custom_components/creality_control/__init__.py
+++ custom_components/creality_control/__init__.py
@@ -21,8 +21,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, 'sensor'))
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, 'button'))
+    await hass.config_entries.async_forward_entry_setups(entry, ['sensor', 'button'])
     return True
 
 class CrealityDataCoordinator(DataUpdateCoordinator):
